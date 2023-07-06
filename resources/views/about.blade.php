125c33f011b554234d7abd9ea5533096e6d5e30e
Wambuiracheal <rachealnjugush@gmail.com>
2023-07-06 17:48:16 +0300

    created the welcome and about page

---
+++ resources/views/about.blade.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>about page</title>
+</head>
+{{------------------------------------style-----------------------------------------}}
+<style>
+#about{
+    padding: 80px 0;
+    color: #f8eaea;
+}
+
+.row{
+    display: flex;
+    justify-content: space-between;
+    word-wrap: wrap;
+    background-color: rgb(141, 29, 122);
+}
+
+.about-col-1{
+    flex-basis: 35%;
+}
+
+.about-col-1 img{
+    width: 100%;
+    border-radius: 10px;
+    width: 100%;
+    height: 100%;
+}
+
+.about-col-2{
+    flex-basis: 60%;
+}
+
+.about-col-2 h1{
+    color: rgb(192, 96, 192);
+}
+
+</style>
+<body>
+<div id="about">
+    <div class="container">
+        <div class="row">
+            <div class="about-col-1">
+                <img src="images/about.jpeg">
+            </div>
+            <div class="about-col-2">
+                <h1>About Us</h1>
+                    <p>Hepi Events is a platform that was specifically designed to host a wide range of events and provide an online booking system for users to conveniently reserve their desired events.
+                    The platform aims to simplify the event booking process, allowing individuals to easily browse through a variety of events and select the ones that suit their preferences.
+                    Hepi Events provides a seamless and user-friendly interface, ensuring a smooth experience for users during the event booking and payment procedures.
+                    By leveraging the power of technology, Hepi Events eliminates the need for traditional offline event booking methods, offering a more efficient and time-saving approach.</p>
+                    <p>The platform was established on the 7th of July, 2023, marking the beginning of its journey in the event management industry.
+                    Hepi Events is owned and managed by Racheal Njuguna, who has extensive experience and expertise in event planning and management.
+                    With Hepi Events, users have the convenience of booking events from the comfort of their own homes, eliminating the need to physically visit event venues or engage in lengthy paperwork.
+                    The platform provides a secure payment system, ensuring that transactions are protected and users' financial information is kept confidential.</p>
+                    <p>Hepi Events offers a diverse range of events, including conferences, seminars, workshops, concerts, exhibitions, and more, catering to various interests and preferences.
+                    Users can easily search for events based on different criteria such as location, date, type, or specific keywords, allowing them to find events that align with their interests and availability.
+                    Hepi Events collaborates with event organizers and venues to ensure a wide selection of high-quality and reputable events are available for booking.
+                    The platform provides detailed event descriptions, including schedules, locations, speakers or performers, ticket prices, and any additional information that users may need to make informed decisions.
+                    Hepi Events promotes community engagement by hosting local and international events, fostering cultural exchange, and providing opportunities for networking and knowledge sharing.</p>
+                    <p>Users can create personalized accounts on Hepi Events, enabling them to save their preferences, track their bookings, and receive notifications about upcoming events.
+                    Hepi Events also offers a feedback system, allowing users to provide reviews and ratings for events they have attended, helping others make informed choices.
+                    The platform continuously updates its event listings to ensure users have access to the latest and most exciting events happening around them.
+                    Hepi Events is committed to delivering exceptional customer service, providing prompt assistance and support to users for any inquiries or issues they may encounter.
+                    The platform embraces innovation and technology advancements in the event management industry, constantly exploring new features and enhancements to improve the user experience.
+                    Hepi Events aims to become a leading platform in the event management industry, known for its reliability, efficiency, and extensive event offerings.
+                    Through Hepi Events, Racheal Njuguna and her team strive to create memorable and enjoyable experiences for event attendees, contributing to the growth and success of the event industry as a whole.</P>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
